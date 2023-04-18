033a4d606f1d94c7d9c90ff562fe49056f8baf91
Hoa Tran Van <hoa.tran4@ntq-solution.com.vn>
2023-04-18 23:29:37 +0700

    HomePage UI

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group(['namespace' => 'Home', 'prefix' => 'home'], function () {
+    Route::get('/',[HomeController::class, 'index']);
 });
