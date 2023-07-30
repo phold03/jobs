61f66f331fc6af9c5045dddcff5e35f670df52e9
Hoa Tran Van <hoa.tran4@ntq-solution.com.vn>
2023-07-30 20:32:29 +0700

    ui client show company

--- resources/views/client/company/index.blade.php
+++ resources/views/client/company/index.blade.php
@@ -1,4 +1,6 @@
-<section>
+@extends('layouts.index')
+@section('home')
+    <section>
         <div class="block no-padding">
             <div class="container fluid">
                 <div class="row">
@@ -23,15 +25,20 @@
             </div>
             <div class="cat-sec">
                 <div class="row no-gape">
+                    @foreach ($company as $item)
                         <div class="col-lg-3 col-md-3 col-sm-6">
                             <div class="p-category">
                                 <a href="{{ route('company.detail', $item->id) }}" title="">
                                     <img class="mt-5" src="{{ asset($item->logo) }}" style="border-radius: 10px"
                                         alt="" width="100">
+                                    <span>{{ $item->name }}</span>
+                                    <p>({{ count($item->employer->job) }} Việc làm)</p>
                                 </a>
                             </div>
                         </div>
+                    @endforeach
                 </div>
             </div>
         </div>
     </section>
+@endsection
