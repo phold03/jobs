108d72a8f344d425f94468a4818e7119d26b489b
tuanmaicong <maicongtuannn@gmail.com>
2023-08-08 22:58:30 +0700

    lichSuGiaoDich

--- app/Models/PaymentHistoryEmployer.php
+++ app/Models/PaymentHistoryEmployer.php
@@ -17,14 +17,14 @@ class PaymentHistoryEmployer extends Model
 {
     /**
      * The table associated with the model.
-     * 
+     *
      * @var string
      */
     protected $table = 'payment_history_emplyer';
 
     /**
      * The "type" of the auto-incrementing ID.
-     * 
+     *
      * @var string
      */
     protected $keyType = 'integer';
