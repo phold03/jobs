588f6860dd8ff637a0e3a2da5ea87bdb0ef1113b
DuongLT <duonglt.work@gmail.com>
2023-08-09 22:50:44 +0700

    update

--- resources/js/components/employer/cv/test.vue
+++ resources/js/components/employer/cv/test.vue
@@ -1,15 +1,4 @@
-<template>
-  <div class="form-group d-flex p-2">
-    <input type="checkbox" @click="cancel" name="check_var" id="check_var" />
-    <label for="check_var" style="margin-left: 5px"
-      >Nếu như bạn không muốn ứng viên nộp hồ sơ vài baif viết của ban!</label
-    >
-  </div>
-  <div class="form-group" v-if="check">
-    <label for="">Tại sao</label>
-    <input type="text" name="reason" v-model="reason" class="form-control" />
-  </div>
-</template>
+
 
 <script>
 export default {
